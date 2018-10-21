48216dd3e61a91dffbb3e4ed7da1c6c6b6051d1e
Joshua Cook <jcook@lynxsolutions.com>
2018-10-20 18:58:31 -0700

    fith push

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,7 +19,7 @@ class MyApp extends App {
             <Container>
                 <ApolloProvider client={apollo}>
                     <Page>
-                        <Component {...pageProps}/>
+                        {/*<Component {...pageProps}/>*/}
                     </Page>
                 </ApolloProvider>
             </Container>
