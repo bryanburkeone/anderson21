0631e3a05ac1f06422c0a3651d375229e6eca71e
Joshua Cook <jcook@lynxsolutions.com>
2018-10-22 18:11:42 -0700

    6pm push

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:7777`;
+export const endpoint = `http://localhost:4444`;
 export const perPage = 4;
+// export const prodEndpoint = 'http://localhost:7777';
 export const prodEndpoint = 'https://sick-fits-yoga-prod-1.herokuapp.com';
